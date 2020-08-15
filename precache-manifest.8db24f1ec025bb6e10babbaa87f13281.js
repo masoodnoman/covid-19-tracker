f94135eff43752a785b85f5c50412d760e337a44
Noman Masood <nomanmasood1234@gmail.com>
2020-08-14 22:35:17 -0400

    Updates

---
+++ precache-manifest.8db24f1ec025bb6e10babbaa87f13281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0184dd313aeb033dd271340a3f8c309a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "132aa74d19b49e5c3820",
+    "url": "/covid-19-tracker/static/css/main.efb5be4d.chunk.css"
+  },
+  {
+    "revision": "333acd6f71726fbbf5b7",
+    "url": "/covid-19-tracker/static/js/2.11a94b9e.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19-tracker/static/js/2.11a94b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132aa74d19b49e5c3820",
+    "url": "/covid-19-tracker/static/js/main.6285237a.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
