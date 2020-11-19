92b6f25143fce0de3f731bb22709ddf2f42edff6
Noman Masood <nomanmasood1234@gmail.com>
2020-11-19 17:19:54 -0500

    Removed Chart and Updated Colors
    Removed the bar chart due to over representation of data, changed background color to maroon, and changed the 'Global' word to white

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
+        {/*<Chart data={data} country={country} /> */}
       </div>
     );
   }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -21,8 +21,8 @@ const CountryPicker = ({ handleCountryChange }) => {
 
     return (
         <FormControl className = {styles.formControl}>
-            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value=""> Global </option>
+            <NativeSelect style={{ color: 'white' }} defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option className = {styles.valueColor} value=""> Global </option>
     {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
